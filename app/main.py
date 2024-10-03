1692fa0961c3fd8bdfb4268241af6da888d6bd8e
Gustavo <penarandagustavo5@gmail.com>
2024-10-03 11:30:45 -0500

    send id=7 in bytes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 import socket  # noqa: F401
 
+def id_to_bytes(id):
+    return id.to_bytes(8)
+
 
 def main():
     
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+
+    while (True):
+        client, addr = server.accept() # wait for client
+        client.send(id_to_bytes(7))
+        client.close()
 
 
 if __name__ == "__main__":
